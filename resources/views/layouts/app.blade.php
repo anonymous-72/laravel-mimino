5bec76ea1413a0631c966d2fd6e471f3a86ca7ec
serghei-bordeniuc <sergey.bordeniuc1@gmail.com>
2024-12-20 13:49:31 +0200

    modified app layout, showed categories list on menu page, installed scrollbar-hide library for tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 
 <x-base-layout :$title>
     <x-layouts.header />
-    <main class="px-6 md:px-12 lg:px-18 mt-28">
+    <main class="px-6 md:px-12 lg:px-18 mt-20 md:mt-24 lg:mt-28">
         {{ $slot }}
     </main>
 </x-base-layout>
--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -3,4 +3,11 @@
         title="{{ __('messages.menu') }}"
         subtitle="{{ __('messages.menu-subtitle') }}"
     />
+    <ul class="mt-3 md:mt-6 lg:mt-12 flex flex-row justify-between gap-1 md:gap-2 overflow-x-auto whitespace-nowrap scrollbar-hide scroll-smooth shadow-lg">
+        @foreach ($categories as $category)
+            <li class="inline-block px-4 py-2 text-xs md:text-base lg:text-xl font-medium rounded-xl cursor-pointer hover:bg-red transition-all duration-300 ease-in-out hover:shadow-2xl">
+                <a href="#">{{ $category->categoryTranslations->first()->name }}</a>
+            </li>
+        @endforeach
+    </ul>
 </x-app-layout>
